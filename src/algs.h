0a2eaf3bdfdb7b017b048b5811d170761bdb77ea
boreddad <ronnebaumfinances@gmail.com>
2023-08-29 11:04:22 -0400

    chore: format

--- src/algs.h
+++ src/algs.h
@@ -7,6 +7,6 @@
 typedef int CmpFn(void* a, void* b);
 
 ssize_t binary_search(void* haystack, void* needle, size_t len,
-                         size_t data_size, CmpFn fn);
+                      size_t data_size, CmpFn fn);
 
 #endif /*__ALGS_H__*/
