e6f59e061674c3090d2ba51d6ef46800045390bd
boreddad <ronnebaumfinances@gmail.com>
2023-08-29 13:59:22 -0400

    fix: void* arithmatic

--- src/binary_search.c
+++ src/binary_search.c
@@ -3,13 +3,14 @@
 #include <sys/types.h>
 
 ssize_t binary_search(void* haystack, void* needle, size_t len,
-                      size_t data_size, CmpFn fn) {
+                      size_t data_size, CmpFn* fn) {
     size_t lo = 0;
     size_t hi = len;
+    unsigned char* h = ((unsigned char*)haystack);
 
     do {
         size_t m = (lo + (hi - lo) / 2);
-        void* v = haystack + (m * data_size);
+        void* v = h + (m * data_size);
         int cmp = fn(v, needle);
         if (cmp == 0) {
             return m;
--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -1,16 +1,17 @@
 #include "algs.h"
 #include <stddef.h>
 
-void bubble_sort(void* arr, size_t len, size_t data_size, CmpFn fn) {
+void bubble_sort(void* arr, size_t len, size_t data_size, CmpFn* fn) {
     size_t i, j;
+    unsigned char* uarr = ((unsigned char*)arr);
 
     for (i = 0; i < len; ++i) {
         size_t jlen = len - 1 - i;
         for (j = 0; j < jlen; ++j) {
             size_t o1 = data_size * j;
             size_t o2 = data_size * (j + 1);
-            void* a = arr + o1;
-            void* b = arr + o2;
+            void* a = uarr + o1;
+            void* b = uarr + o2;
             int cmp = fn(a, b);
             if (cmp > 0) {
                 swap(a, b, data_size);
