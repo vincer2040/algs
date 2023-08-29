4d9c24b6378d48f794867534dccd8cef9b842f9a
boreddad <ronnebaumfinances@gmail.com>
2023-08-29 12:59:45 -0400

    feat: bubble sort

--- src/algs.h
+++ src/algs.h
@@ -4,9 +4,23 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+#define swap(a1, b1, s)                                                        \
+    {                                                                          \
+        size_t k;                                                              \
+        unsigned char* uca = ((unsigned char*)a1);                             \
+        unsigned char* ucb = ((unsigned char*)b1);                             \
+        for (k = 0; k < s; ++k) {                                              \
+            unsigned char tmp = uca[k];                                        \
+            uca[k] = ucb[k];                                                   \
+            ucb[k] = tmp;                                                      \
+        }                                                                      \
+    }
+
 typedef int CmpFn(void* a, void* b);
 
 ssize_t binary_search(void* haystack, void* needle, size_t len,
                       size_t data_size, CmpFn fn);
+
+void bubble_sort(void* arr, size_t len, size_t data_size, CmpFn fn);
 
 #endif /*__ALGS_H__*/
